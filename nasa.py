1bf5b2503701612683249847e8f53b25ded827ac
hamzadxtr1 <149436923+hamzadxtr1@users.noreply.github.com>
2023-11-03 22:00:45 +0100

    Add files via upload

---
+++ nasa.py
@@ -0,0 +1,22 @@
+
+import time
+first_number = input('enter your first number ')
+time.sleep(0.5)
+second_numbe = input('enter your second nuber ')
+time.sleep(0.5)
+answer = input('what is 7 + 8 = ')
+if answer == '15':
+    time.sleep(0.5) 
+    print ('congratulations you hacked NASA ')
+else:
+    print('bro are fuckin dumb i will give you another one ')
+    time.sleep(0.5)
+    print('i will give you another one')
+    time.sleep(0.5)
+    answer_2 = input("what's 3 + 5 = ")
+    if answer_2 == '8':
+        print('finally')
+        time.sleep(0.5)
+        print('and yeah i forgot some think')
+        time.sleep(0.3)
+        print('congratulations you hacked NASA ')
